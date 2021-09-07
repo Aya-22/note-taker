78378a59107d8eeb1c2e0a1d4fcafeedd3df2c16
Jennifer Ayala <j.ayala0695@gmail.com>
2021-09-07 16:39:03 -0700

    update to delete function

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const path = require('path');
 let database = require('./db/db.json');
 const shortid = require('shortid');
 const fs = require('fs');
+const { get } = require('http');
 
 const app = express();
 const PORT = 3000;
 
-app.use(express.urlencoded({ extended:true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static('public'));
@@ -15,7 +16,7 @@ app.use(express.static('public'));
 // console.log(database);
 
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
 
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
 
@@ -37,7 +38,12 @@ app.post('/api/notes', (req,res) => {
         }; 
         res.json(database)
     })
-})
+});
 
+app.delete('/api/notes/:id', (req, res) => {
+    const notesId = Number(req.params.id);
+    
+
+})
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
